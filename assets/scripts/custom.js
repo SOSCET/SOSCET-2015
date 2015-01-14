fa594592d9c518421ce87ed543722e5483713e09
BinotaLiu <binota@binota.org>
2015-01-14 21:49:09 +0800

    目錄整理
    assets  目錄

---
+++ assets/scripts/custom.js
@@ -0,0 +1,44 @@
+//取得瀏覽器視窗高度
+function getBrowserHeight() {
+      return window.innerHeight;
+}
+
+$(document).ready(function(){
+  var navHeight = $('#nav').height();
+  var sx = $('#header').width();
+  var sy = $('#header').height();
+
+  //滾動事件
+  $(window).scroll(function() {
+    var headerHeight = $('#header').height() - navHeight;
+    var scrollTopHeight = $(window).scrollTop();
+        
+    if(scrollTopHeight > headerHeight) {
+      $('#nav').addClass('navbar-fixed-top');
+      $('#wrapper').css({marginTop: navHeight * 1.5});
+    } else {
+      $('#nav').removeClass('navbar-fixed-top');
+      $('#wrapper').css({marginTop: "0px"});
+    }
+  });
+
+  //body附加滾動監視
+  $('body').scrollspy({target: '#nav', offset: navHeight});
+
+  //點擊錨點的平滑滾動
+  $('.presentation > a').click(function(){
+    $('html').animate({scrollTop: ($($(this).attr("href")).offset().top - navHeight)});
+    return false;
+  });
+
+  $(document).mousemove(function(e) {
+    var cx = e.clientX; var cy = e.clientY;
+    var offset = 10;
+
+    var ox = (cx / sx);
+    var oy = (cy / sy);
+
+    $('#header').css({ backgroundPosition: (ox * offset) + '%' + ' ' + (oy * offset) + '%' });
+  });
+});
+

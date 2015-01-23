cafa38b7a740a64d78011e9db9b1b2f4427593ed
unknown <gtrx8fd3ds@gmail.com>
2015-01-23 13:03:29 +0800

    google map added

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -34,3 +34,21 @@ $(document).ready(function(){
   });
 });
 
+var map;
+var myLatlng = new google.maps.LatLng(23.999612, 121.62038);
+function initialize() {
+  var mapOptions = {
+    zoom: 17,
+    center: myLatlng
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: "Hello World!"
+});
+
+google.maps.event.addDomListener(window, 'load', initialize);
